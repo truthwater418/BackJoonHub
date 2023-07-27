83f0cc2bfe2b903c7ff56db7c972deb282856df4
truthwater418 <140360547+truthwater418@users.noreply.github.com>
2023-07-27 17:15:40 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 25.14 ms, Memory: 112 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int []arr) {
+        List<Integer> list = new ArrayList<>();
+        list.add(arr[0]);
+        if(arr.length != 1){
+            for(int i = 1; i < arr.length; i++){
+                if(arr[i - 1] != arr[i]){
+                    list.add(arr[i]);  
+                }
+            }
+        }else{
+            return arr;
+        }
+        int[] answer = new int[list.size()];
+        for(int i = 0; i < list.size(); i++){
+            answer[i] = list.get(i);
+        }
+        return answer;
+    }
+}
